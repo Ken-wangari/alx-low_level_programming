f3de2ea62c2abb59dd7ae1a52a1a7e0386eff688
Ken-wangari <wangariken50@gmail.com>
2023-07-14 09:17:03 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1;
+
+	int num2;
+	for (num1 = 0; num1 < 99; num1++) {
+		for (num2 = num1 + 1; num <= 99; num2++) {
+
+			putchar(num1/10 + '0');
+			putchar((num1%10) + '0');
+			putchar(' ');
+			putchar(num1/10 + '0');
+			putchar((num2%10) + '0');
+
+			if (num1 == 7 && num2 8 && num3 == 9)
+				continue;
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	putchar('\n');
+
+	return (0);
+}
