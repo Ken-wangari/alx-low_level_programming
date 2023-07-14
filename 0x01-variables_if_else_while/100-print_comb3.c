2eda8d0a47d96a42b8e8d094f308a64705ef5c54
Ken-wangari <wangariken50@gmail.com>
2023-07-14 08:24:10 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Ken-wangari
+ */
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digital1, digital2;
+	
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit 2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
