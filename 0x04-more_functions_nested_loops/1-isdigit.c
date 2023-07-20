c8b84f1abf52dfff181b348b7a6b932a41fdf443
Ken-wangari <wangariken50@gmail.com>
2023-07-20 09:11:37 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigi-func checks for a digit (0 thru 9)
+ * @c: inter to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
